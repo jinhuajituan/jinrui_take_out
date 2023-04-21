8d4a0685f3df3061597ee4512a26812e3d51c875
AFSFDS <TestGit>
2023-04-21 08:26:13 +0800

    更新拦截器Bug

--- src/main/resources/backend/js/request.js
+++ src/main/resources/backend/js/request.js
@@ -44,13 +44,19 @@
 
   // 响应拦截器
   service.interceptors.response.use(res => {
-      if (res.data.code === 0 && res.data.msg === 'NOTLOGIN') {// 返回登录页面
-        console.log('---/backend/page/login/login.html---')
-        localStorage.removeItem('userInfo')
-        window.top.location.href = '/backend/page/login/login.html'
-      } else {
-        return res.data
-      }
+        console.log('---响应拦截器---',res)
+        // 未设置状态码则默认成功状态
+        const code = res.data.code;
+        // 获取错误信息
+        const msg = res.data.msg
+        console.log('---code---',code)
+        if (res.data.code === 0 && res.data.msg === 'NOTLOGIN') {// 返回登录页面
+          console.log('---/backend/page/login/login.html---',code)
+          localStorage.removeItem('userInfo')
+          window.top.location.href = '/backend/page/login/login.html'
+        } else {
+          return res.data
+        }
     },
     error => {
       console.log('err' + error)
