d899500b66c9f09a1a3f67316d5e85aa4a56da4a
AFSFDS <TestGit>
2023-04-25 08:33:22 +0800

    通过验证码登录

--- src/main/java/com/jinli/jinrui/controller/UserController.java
+++ src/main/java/com/jinli/jinrui/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.jinli.jinrui.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.jinli.jinrui.common.Result;
 import com.jinli.jinrui.entity.User;
 import com.jinli.jinrui.service.UserService;
+import com.jinli.jinrui.utils.SMSUtils;
 import com.jinli.jinrui.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -45,6 +48,43 @@ public class UserController {
             return Result.success("手机短信验证码发送成功");
         }
         return Result.error("短信验证码发送失败");
+    }
+
+    /***
+     * 手机验证登录
+     * @param map
+     * @param session
+     * @return
+     */
+    @PostMapping("/login")
+    public Result<User> login(@RequestBody Map map , HttpSession session){
+        log.info(map.toString());
+        //获取手机号
+        String phone = map.get("phone").toString();
+        //获取验证码
+        String code = map.get("code").toString();
+        //从Session中获取保存的验证码
+        Object codeInSession = session.getAttribute(phone);
+        //进行验证码的比对(页面提交的验证码和Session中保存的验证码比对)
+        if(codeInSession != null && codeInSession.equals(code)){
+            //如果能够比对成功,说明登录成功
+
+
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+
+            User user = userService.getOne(queryWrapper);
+            if(user == null){
+                //判断当前手机号对应的用户是否为新用户,如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+            session.setAttribute("user", user.getId());
+            return Result.success(user);
+        }
+        return Result.error("登录失败");
     }
 
 
